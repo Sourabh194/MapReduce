29355fe610b928e85ba0ca1cab1ded74c82e8996
Sourabh194 <35278101+Sourabh194@users.noreply.github.com>
2018-01-09 18:04:11 -0600

    Update Reducer1.py

--- Reducer1.py
+++ Reducer1.py
@@ -6,15 +6,11 @@ import re
 index = {}
 	 
 for line in sys.stdin:
-	word, postings = line.split('\t')
-	postings = postings.split('\n')[0]
-	index.setdefault(word,[])        
-	#for posting in postings.split(','):
-        #file, count = postings.split(':')
-	      #  count = int(count) 
-	if postings not in index[word]:
-		index[word].append(postings)
-	      #  index[word][file] += count
-  
+	word, fileName = line.split('\t')
+	fileName = fileName.split('\n')[0]
+	index.setdefault(word,[])
+	if FileName not in index[word]:
+		index[word].append(FileName)
+		
 for word in index:
 	sys.stdout.write("{0}\t{1}\n".format(word, index[word]))
